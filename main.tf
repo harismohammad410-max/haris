40af14fdfdc83a6d8ceaff60dcdc6cf416140d9d
Muhammad Haris <mharis.sajid@systemsltd.com>
2025-10-27 03:29:43 -0700

    first commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.100.0"
+    }
+  }
+  required_version = ">=1.5.0"
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id="3221f75f-406b-41cb-8e80-7dcf8c8aa499"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-logicapp-private"
+  location = "East US"
+}
